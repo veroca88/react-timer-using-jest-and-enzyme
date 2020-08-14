77f0e48816047b36980af4e229d1ddd7a8795fdf
veroca88 <veronicadonoso1506@gmail.com>
2020-08-14 10:26:08 -0400

    Remove global style and define onClick function on button

--- src/components/TimerButton/TimerButton.jsx
+++ src/components/TimerButton/TimerButton.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./TimerButton.css";
 
 const TimerButton = ({ buttonAction, buttonValue }) => (
-  <div className="button-container" />
+  <div className="button-container" onClick={() => buttonAction()}>
+    <p className="button-value">{buttonValue}</p>
+  </div>
 );
 
 TimerButton.propTypes = {
