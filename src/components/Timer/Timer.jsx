ca45eeedac9c459dde6703d99389b7bbd5ff6a33
veroca88 <veronicadonoso1506@gmail.com>
2020-08-14 10:49:49 -0400

    Do functionality of timer

--- src/components/Timer/Timer.jsx
+++ src/components/Timer/Timer.jsx
@@ -11,27 +11,75 @@ class Timer extends Component {
       seconds: 0,
       isOn: false,
     };
+
+    this.startTimer = this.startTimer.bind(this);
+    this.stopTimer = this.stopTimer.bind(this);
+    this.resetTimer = this.resetTimer.bind(this);
   }
 
   startTimer() {
-    console.log("Starting timer.");
+    if (this.state.isOn === true) {
+      return;
+    }
+    this.myInterval = setInterval(() => {
+      const { seconds, minutes } = this.state;
+
+      if (seconds > 0) {
+        this.setState(({ seconds }) => ({
+          seconds: seconds - 1,
+        }));
+      }
+      if (seconds === 0) {
+        if (minutes === 0) {
+          clearInterval(this.myInterval);
+        } else {
+          this.setState(({ minutes }) => ({
+            minutes: minutes - 1,
+            seconds: 59,
+          }));
+        }
+      }
+    }, 1000);
+    this.setState({ isOn: true });
   }
 
   stopTimer() {
-    console.log("Stopping timer.");
+    clearInterval(this.myInterval);
+    this.setState({ isOn: false });
   }
 
   resetTimer() {
-    console.log("Resetting timer.");
+    this.stopTimer();
+    this.setState({
+      minutes: 25,
+      seconds: 0,
+    });
   }
+
   render = () => {
+    const { minutes, seconds } = this.state;
+
     return (
       <div className="timer-container">
-        <div className="timer-display"></div>
+        <div className="time-display">
+          {minutes}:{seconds < 10 ? `0${seconds}` : seconds}
+        </div>
         <div className="timer-button-container">
-          <TimerButton buttonAction={this.startTimer} buttonValue={"Start"} />
-          <TimerButton buttonAction={this.stopTimer} buttonValue={"Stop"} />
-          <TimerButton buttonAction={this.resetTimer} buttonValue={"Reset"} />
+          <TimerButton
+            className="start-timer"
+            buttonAction={this.startTimer}
+            buttonValue={"Start"}
+          />
+          <TimerButton
+            className="stop-timer"
+            buttonAction={this.stopTimer}
+            buttonValue={"Stop"}
+          />
+          <TimerButton
+            className="reset-timer"
+            buttonAction={this.resetTimer}
+            buttonValue={"Reset"}
+          />
         </div>
       </div>
     );
--- src/components/Timer/Timer.spec.js
+++ src/components/Timer/Timer.spec.js
@@ -16,7 +16,7 @@ describe("mounted Timer", () => {
   });
 
   it("invokes startTimer when the start button is clicked", () => {
-    const jestSpy = jest.spyOn(container.instance(), "startTimer");
+    const spy = jest.spyOn(container.instance(), "startTimer");
     container.instance().forceUpdate();
     expect(spy).toHaveBeenCalledTimes(0);
     container.find(".start-timer").first().simulate("click");
@@ -24,7 +24,7 @@ describe("mounted Timer", () => {
   });
 
   it("invokes stopTimer when the stop button is clicked", () => {
-    const jestSpy = jest.spyOn(container.instance(), "stopTimer");
+    const spy = jest.spyOn(container.instance(), "stopTimer");
     container.instance().forceUpdate();
     expect(spy).toHaveBeenCalledTimes(0);
     container.find(".stop-timer").first().simulate("click");
@@ -32,10 +32,30 @@ describe("mounted Timer", () => {
   });
 
   it("invokes resetTimer when the reset button is clicked", () => {
-    const jestSpy = jest.spyOn(container.instance(), "resetTimer");
+    const spy = jest.spyOn(container.instance(), "resetTimer");
     container.instance().forceUpdate();
     expect(spy).toHaveBeenCalledTimes(0);
     container.find(".reset-timer").first().simulate("click");
     expect(spy).toHaveBeenCalledTimes(1);
+  });
+
+  it("should change isOn state true when the start button is clicked", () => {
+    container.instance().forceUpdate();
+    container.find(".start-timer").first().simulate("click");
+    expect(container.instance().state.isOn).toEqual(true);
+  });
+
+  it("should change isOn state false when the stop button is clicked", () => {
+    container.instance().forceUpdate();
+    container.find(".stop-timer").first().simulate("click");
+    expect(container.instance().state.isOn).toEqual(false);
+  });
+
+  it("should change isOn state false when the reset button is clicked", () => {
+    container.instance().forceUpdate();
+    container.find(".stop-timer").first().simulate("click");
+    expect(container.instance().state.isOn).toEqual(false);
+    expect(container.instance().state.minutes).toEqual(25);
+    expect(container.instance().state.seconds).toEqual(0);
   });
 });
